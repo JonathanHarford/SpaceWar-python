95921f877c14b71297afc601a8dd2947b00b5ec4
Jonathan Harford <jonathan.harford@gmail.com>
2013-04-26 10:09:21 -0400

    Test harness functioning!
    Did it really work before? It doesn't make sense that it would.

--- test/spacewar_func_test.py
+++ test/spacewar_func_test.py
@@ -5,17 +5,33 @@ import unittest
 import sys
 
 sys.path.append("../")
-### sys.path.append("../../")
+
 
 from spacewar_func import *  # My Spacewar functions
 
 class TestSun(unittest.TestCase):
 
     def setUp(self):
-        testsun = Sun()
+        pygame.init()
+        testgamestate = GameState()
+        screen = pygame.display.set_mode((DISP_WIDTH, DISP_HEIGHT))
+        self.testsun = Sun(testgamestate, load_image("ball.png"), (400,300))
 
-    def test_Sun_has_mass_by_default(self):
-        self.assertGreater(testsun.mass, 0)
+    def test_sun_has_mass_by_default(self):
+        print(self.testsun.mass)
+        self.assertGreater(self.testsun.mass, 0)
+
+    def tearDown(self):
+        pygame.quit()
+
+##class TestShip(unittest.TestCase):
+##
+##    def setUp(self):
+##        testgamestate = GameState()
+##        testship = Ship(testgamestate, load_image(r"..\data\ship.png"), (200,200), (-3,3))
+##
+##    def test_ship_cant_shoot_after_death(self):
+
 
 if __name__ == "__main__":
     unittest.main()
